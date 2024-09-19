480d7486ed4d5431473a1fc26e0c93b47d4af2cc
yevhen.mediakov <yevhen.mediakov@globallogic.com>
2024-09-19 18:53:54 +0300

    initial commit

---
+++ lib/resources/app_colors.dart
@@ -0,0 +1,8 @@
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color red  = Colors.red;
+  static const Color blue  = Colors.blue;
+  static const Color black  = Colors.black;
+}
