aae70f1479a18d6bb583b5b92d71298140638e30
yevhen.mediakov <yevhen.mediakov@globallogic.com>
2024-09-27 10:24:57 +0300

    TST-8.2: remove useless async await

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:test_prj/presentation/app/test_app.dart';
 
-void main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(TestApp());
